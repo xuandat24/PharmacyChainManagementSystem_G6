3bd972877b933ef50982c7c24f9227eef43cb551
minthecat44 <firedeath576@gmail.com>
2026-07-17 08:29:01 +0700

    Code done

--- PharmacyManagement/src/main/java/fu/se/pharmacy/dto/BranchDTO.java
+++ PharmacyManagement/src/main/java/fu/se/pharmacy/dto/BranchDTO.java
@@ -1,5 +1,7 @@
 package fu.se.pharmacy.dto;
 
+import fu.se.pharmacy.validator.ValidPhone;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -8,6 +10,9 @@ public class BranchDTO {
     private String branchCode;
     private String branchName;
     private String address;
+
+    @ValidPhone
+    @Size(max = 20, message = "Số điện thoại tối đa 20 ký tự")
     private String phone;
     private String status;
 }
